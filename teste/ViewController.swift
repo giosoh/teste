5f215d72d30486478d7c277f3ecc31e3c4392c2e
giosoh <giosoh>
2022-07-04 10:27:17 -0300

    adiciona projeto do xcode e realiza primeiro commit

---
+++ teste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teste
+//
+//  Created by Giovanna Paz on 04/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
